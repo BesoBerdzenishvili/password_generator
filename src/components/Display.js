361849ccd1af46b9ba7ad3db19d59c3f884931c7
fliderman01 <fliderman01@gmail.com>
2022-10-04 13:51:25 +0400

    added Display.js and CopyBtn.js

---
+++ src/components/Display.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { styled, globalStyles } from "../stitches.config";
+import CopyBtn from "./CopyBtn";
+
+const Wrapper = styled("div", {
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  backgroundColor: "#24232b",
+  padding: "0 18px",
+  width: "384px",
+  height: "58px",
+  color: "#3c3b43",
+  fontSize: "26px",
+  fontWeight: "bolder",
+});
+
+globalStyles();
+export default function Display({ password = "P4$W0rD!" }) {
+  return (
+    <Wrapper>
+      <p>{password}</p>
+      <CopyBtn text={password} />
+    </Wrapper>
+  );
+}
